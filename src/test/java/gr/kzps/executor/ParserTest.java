8738c7f317abee0a83cfcafaf8f3a64216e0372c
alafroiskiotos <kouzoupis.ant@gmail.com>
2015-05-23 18:49:09 +0200

    Add license

--- src/test/java/gr/kzps/executor/ParserTest.java
+++ src/test/java/gr/kzps/executor/ParserTest.java
@@ -1,3 +1,22 @@
+/*	
+	Copyright (C) 2015
+ 	Antonis Kouzoupis <kouzoupis.ant@gmail.com>
+
+	This file is part of FileCrypto.
+
+    FileCrypto is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FileCrypto is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FileCrypto.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package gr.kzps.executor;
 
 import org.apache.commons.cli.CommandLine;
@@ -19,7 +38,7 @@ public class ParserTest {
 	@BeforeClass
 	public static void before() {
 		String[] args = {"-e", "-i input/test", "-o output/test", "-h", "--version", "-k keys/key"};
-		
+				
 		parser = new CommandParser(args);
 		try {
 			cmd = parser.parseArgs();
--- src/test/java/gr/kzps/filesystem/FileWriterTest.java
+++ src/test/java/gr/kzps/filesystem/FileWriterTest.java
@@ -1,3 +1,22 @@
+/*	
+	Copyright (C) 2015
+ 	Antonis Kouzoupis <kouzoupis.ant@gmail.com>
+
+	This file is part of FileCrypto.
+
+    FileCrypto is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FileCrypto is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FileCrypto.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package gr.kzps.filesystem;
 
 import java.io.File;
@@ -13,7 +32,7 @@ import static org.junit.Assert.assertEquals;
 
 public class FileWriterTest {
 	private static FilesystemOperations fso;
-	private final static String newFile = "/home/antonis/Tobii/FileCrypto/output_test/writeTest";
+	private final static String newFile = "output_test/writeTest";
 	private final static String content = "output write test!";
 	private static File file;
 
