68f8e5125b18f7bf6919cd1e045e21136fd87f1f
alafroiskiotos <kouzoupis.ant@gmail.com>
2015-06-10 18:23:08 +0200

    src/main/java/gr/kzps/FileCrypto/crypto/RSACipher.java

--- src/main/java/gr/kzps/FileCrypto/crypto/AESPrimitives.java
+++ src/main/java/gr/kzps/FileCrypto/crypto/AESPrimitives.java
@@ -1,3 +1,22 @@
+/*	
+	Copyright (C) 2015
+ 	Antonis Kouzoupis <kouzoupis.ant@gmail.com>
+
+	This file is part of FileCrypto.
+
+    FileCrypto is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FileCrypto is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FileCrypto.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package gr.kzps.FileCrypto.crypto;
 
 import java.io.Serializable;
@@ -5,17 +24,17 @@ import java.io.Serializable;
 public class AESPrimitives implements Serializable{
 	private static final long serialVersionUID = 5380589340429089751L;
 
-	private final char[] password;
+	private final byte[] password;
 	private final byte[] seed;
 	private final byte[] salt;
 	
-	public AESPrimitives(char[] password, byte[] seed, byte[] salt) {
+	public AESPrimitives(byte[] password, byte[] seed, byte[] salt) {
 		this.password = password;
 		this.seed = seed;
 		this.salt = salt;
 	}
 	
-	public char[] getPassword() {
+	public byte[] getPassword() {
 		return password;
 	}
 	
--- src/main/java/gr/kzps/FileCrypto/executor/Dispatcher.java
+++ src/main/java/gr/kzps/FileCrypto/executor/Dispatcher.java
@@ -16,13 +16,14 @@
 
     You should have received a copy of the GNU General Public License
     along with FileCrypto.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 package gr.kzps.FileCrypto.executor;
 
 import gr.kzps.FileCrypto.crypto.AESDecrypt;
 import gr.kzps.FileCrypto.crypto.AESEncrypt;
 import gr.kzps.FileCrypto.crypto.AESPrimitives;
 import gr.kzps.FileCrypto.crypto.CryptoOperation;
+import gr.kzps.FileCrypto.crypto.RSACipher;
 import gr.kzps.FileCrypto.crypto.RSADecrypt;
 import gr.kzps.FileCrypto.crypto.RSAEncrypt;
 import gr.kzps.FileCrypto.exceptions.NoCryptoKeyProvided;
@@ -33,10 +34,16 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream.GetField;
 import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
 import java.nio.file.NotDirectoryException;
 import java.nio.file.Paths;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -54,7 +61,7 @@ import org.apache.logging.log4j.Logger;
  */
 public class Dispatcher {
 	private static final Logger log = LogManager.getLogger(Dispatcher.class);
-	
+
 	private static final String META_FILENAME = "FileCrypto_AESmetadata";
 	private static List<Runnable> tasks;
 	private static FilesystemOperations fso;
@@ -63,10 +70,14 @@ public class Dispatcher {
 	/**
 	 * Static method that dispaches files to encryption/decryption threads
 	 * 
-	 * @param operation Encryption or Decryption operation
-	 * @param inputDirectory Directory where the files to be encrypted/decrypted reside
-	 * @param outputDirectory Output directory of the cryptographics operation
-	 * @param cryptoKey Cryptographic key
+	 * @param operation
+	 *            Encryption or Decryption operation
+	 * @param inputDirectory
+	 *            Directory where the files to be encrypted/decrypted reside
+	 * @param outputDirectory
+	 *            Output directory of the cryptographics operation
+	 * @param cryptoKey
+	 *            Cryptographic key
 	 * @return Number of dispatched lists with files. For testing purposes
 	 * @throws FileNotFoundException
 	 * @throws NotDirectoryException
@@ -74,20 +85,22 @@ public class Dispatcher {
 	 * @throws NoCryptoKeyProvided
 	 */
 	public static int dispatch(CryptoOperation operation,
-			String inputDirectory, String outputDirectory, Integer threshold, String cryptoKey)
-			throws FileNotFoundException, NotDirectoryException,
-			InterruptedException, NoCryptoKeyProvided {
+			String inputDirectory, String outputDirectory, Integer threshold,
+			String cryptoKey) throws FileNotFoundException,
+			NotDirectoryException, InterruptedException, NoCryptoKeyProvided {
 
 		fso = new FilesystemOperations();
 		// TODO Implement option for excluding files
 		List<String> excludeFiles = new ArrayList<String>();
 		excludeFiles.add(META_FILENAME);
-		List<File> inputFiles = fso.enumerateInputFiles(inputDirectory, excludeFiles);
+		List<File> inputFiles = fso.enumerateInputFiles(inputDirectory,
+				excludeFiles);
 		tasks = new ArrayList<Runnable>();
 		rand = new SecureRandom();
 		char[] password = null;
 		byte[] seed = null;
-		byte [] salt = null;
+		byte[] salt = null;
+
 		byte[] key = readCryptoKey(cryptoKey);
 
 		// For testing
@@ -101,7 +114,7 @@ public class Dispatcher {
 		File output = new File(outputDirectory);
 		if (!output.exists())
 			output.mkdir();
-		
+
 		// TODO Construct or recover AES primitives
 		if (operation.equals(CryptoOperation.ENCRYPT)) {
 			log.debug("Construct AES primitives");
@@ -111,36 +124,52 @@ public class Dispatcher {
 			log.debug("Generating salt");
 			salt = rand.generateSeed(32);
 
-			AESPrimitives primitives = new AESPrimitives(password, seed, salt);
-			String absoluteName = Paths.get(outputDirectory, META_FILENAME).toString();
-			log.debug(absoluteName);
+			RSACipher<PublicKey> rsaCipher = new RSACipher<PublicKey>(
+					CryptoOperation.ENCRYPT, key);
+			byte[] encryptedPass = rsaCipher.encrypt(toBytes(password));
+			byte[] encryptedSeed = rsaCipher.encrypt(seed);
+			byte[] encryptedSalt = rsaCipher.encrypt(salt);
+
+			AESPrimitives primitives = new AESPrimitives(encryptedPass,
+					encryptedSeed, encryptedSalt);
+			String absoluteName = Paths.get(outputDirectory, META_FILENAME)
+					.toString();
 			fso.serializeObject(primitives, new File(absoluteName));
-			
+
 			// TODO RSA Encrypt metadata
 		} else if (operation.equals(CryptoOperation.DECRYPT)) {
 			log.debug("Recover AES primitives");
 			// TODO RSA Decrypt metadata
-			
-			String absoluteName = Paths.get(inputDirectory, META_FILENAME).toString();
-			AESPrimitives primitives = fso.deserializeObject(new File(absoluteName));
-			password = primitives.getPassword();
-			seed = primitives.getSeed();
-			salt = primitives.getSalt();
+
+			String absoluteName = Paths.get(inputDirectory, META_FILENAME)
+					.toString();
+			AESPrimitives primitives = fso.deserializeObject(new File(
+					absoluteName));
+			byte[] encryptedPass = primitives.getPassword();
+			byte[] encryptedSeed = primitives.getSeed();
+			byte[] encryptedSalt = primitives.getSalt();
+
+			RSACipher<PrivateKey> rsaCipher = new RSACipher<PrivateKey>(
+					CryptoOperation.DECRYPT, key);
+			password = toChar(rsaCipher.decrypt(encryptedPass));
+			seed = rsaCipher.decrypt(encryptedSeed);
+			salt = rsaCipher.decrypt(encryptedSalt);
 		}
-		
-			
+
 		log.info("Started with threshold: {}", threshold);
-		
+
 		if (inputFiles.size() < threshold) {
 			// Dispatch all files to one processor
 			log.info("Dispatch files to one thread");
 			// Dispatch list to worker thread
 			if (operation.equals(CryptoOperation.ENCRYPT)) {
 				// TODO AES Encrypt
-				tasks.add(new AESEncrypt(password, seed, salt, inputFiles, outputDirectory));
+				tasks.add(new AESEncrypt(password, seed, salt, inputFiles,
+						outputDirectory));
 			} else if (operation.equals(CryptoOperation.DECRYPT)) {
 				// TODO AES Decrypt
-				tasks.add(new AESDecrypt(password, seed, salt, inputFiles, outputDirectory));
+				tasks.add(new AESDecrypt(password, seed, salt, inputFiles,
+						outputDirectory));
 			}
 
 			dispatchedLists++;
@@ -170,16 +199,18 @@ public class Dispatcher {
 				// Dispatch lists to worker threads
 				if (operation.equals(CryptoOperation.ENCRYPT)) {
 					// TODO AES Encrypt
-					tasks.add(new AESEncrypt(password, seed, salt, inputFiles, outputDirectory));
+					tasks.add(new AESEncrypt(password, seed, salt, inputFiles,
+							outputDirectory));
 				} else if (operation.equals(CryptoOperation.DECRYPT)) {
 					// TODO AES Decrypt
-					tasks.add(new AESDecrypt(password, seed, salt, inputFiles, outputDirectory));
+					tasks.add(new AESDecrypt(password, seed, salt, inputFiles,
+							outputDirectory));
 				}
 			}
 		}
-		
+
 		long startTime = System.currentTimeMillis();
-		
+
 		if (tasks.size() > 0) {
 			// Spawn threads
 			tasks.stream().forEach(x -> execService.execute(x));
@@ -202,18 +233,51 @@ public class Dispatcher {
 	}
 
 	/**
+	 * Convert byte array to char array
+	 * 
+	 * @param bytes
+	 *            Password byte array
+	 * @return Password char array
+	 */
+	private static char[] toChar(byte[] bytes) {
+		ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+		CharBuffer charBuffer = Charset.forName("UTF-8").decode(byteBuffer);
+
+		return Arrays.copyOfRange(charBuffer.array(), 0, charBuffer.limit());
+	}
+
+	/**
+	 * Convert password to byte array
+	 * 
+	 * @param chars
+	 *            Password
+	 * @return Password in byte array
+	 */
+	private static byte[] toBytes(char[] chars) {
+		CharBuffer charBuffer = CharBuffer.wrap(chars);
+		ByteBuffer byteBuffer = Charset.forName("UTF-8").encode(charBuffer);
+		byte[] bytes = Arrays.copyOfRange(byteBuffer.array(),
+				byteBuffer.position(), byteBuffer.limit());
+		Arrays.fill(charBuffer.array(), '\u0000'); // clear sensitive data
+		Arrays.fill(byteBuffer.array(), (byte) 0); // clear sensitive data
+		return bytes;
+	}
+
+	/**
 	 * Generate random password
+	 * 
 	 * @return password
 	 */
 	private static char[] generatePassword() {
 		log.debug("Generating password");
 		return new BigInteger(130, rand).toString(32).toCharArray();
 	}
-	
+
 	/**
 	 * Read cryptographic key from disk
 	 * 
-	 * @param key Path to the key, parsed from command line argument
+	 * @param key
+	 *            Path to the key, parsed from command line argument
 	 * @return Cryptographic key
 	 * @throws NoCryptoKeyProvided
 	 */
